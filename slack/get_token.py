c6161e7e38d1433c556dab1755ef7a103ae74d09
catowabisabi <catowabisabi@gmail.com>
2023-05-02 01:25:33 -0400

    first commit

---
+++ slack/get_token.py
@@ -0,0 +1,42 @@
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_bolt import App
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, request
+from functions import draft_email
+
+# Load environment variables from .env file
+load_dotenv(find_dotenv())
+
+# Set Slack API credentials
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
+SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
+
+# Initialize the Slack app
+app = App(token=SLACK_BOT_TOKEN)
+
+# Initialize the Flask app
+# Flask is a web application framework written in Python
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+
+def get_bot_user_id():
+    """
+    Get the bot user ID using the Slack API.
+    Returns:
+        str: The bot user ID.
+    """
+    try:
+        # Initialize the Slack client with your bot token
+        slack_client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+        response = slack_client.auth_test()
+        return response["user_id"]
+    except SlackApiError as e:
+        print(f"Error: {e}")
+
+user_id = get_bot_user_id()
+print (user_id)
